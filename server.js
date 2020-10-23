ea8ad47bc975ea390548d08ecf3c99215453aa32
stealthman22 <tdotonline@gmail.com>
2020-10-23 16:01:06 +0000

    scaffolded basic routes setupu

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ const PORT = process.env.PORT || 6005;
 // connect db
 connectDB();    
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/auth', require('./routes/api/auth'))
+
 app.listen(PORT, () => console.log(`Application running on ${PORT}`) );
