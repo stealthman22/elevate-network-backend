3415b92b9cb3752fa0008e2a690aa6fa40d66dcf
stealthman22 <tdotonline@gmail.com>
2020-10-26 18:32:06 +0000

    built custom middleware for user authentication for json web tokens

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// authentication logic
+module.exports = function(req, res, next)  {
+    const token = req.headers('x-auth-token'); 
+    
+    // if no token is returned
+    if(!token) {
+        return res.status(401).json({msg: 'No token, permission denied'})
+    }
+
+    // if returned, verify token
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'))
+        // set decoded user param to request's 
+        req.user = decoded.user
+        next()
+    } catch (error) {
+        res.status(401).json({msg: 'Wrong token, authentication failed'})
+    }
+}
