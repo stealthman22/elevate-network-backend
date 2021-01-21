8de97caf8ec58fd3813f588bb6c42b481903ceb3
stealthman22 <tdotonline@gmail.com>
2021-01-21 00:22:12 +0000

    modified endpoints

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use('/admin', require('./admin/adminDashboard'));
 app.use('/api/user', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profileRouter'));
+// individual profile routes
 app.use('/api/menteeProfile', require('./routes/api/menteeProfile'));
 app.use('/api/mentorProfile', require('./routes/api/mentorProfile'));
 app.use('/api/partnerProfile', require('./routes/api/partnerProfile'));
