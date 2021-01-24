c460fc331e55784df64d935e632381919dada10f
stealthman22 <tdotonline@gmail.com>
2021-01-24 18:28:57 +0000

    few changes to server code

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.json({ extended: false }));
 app.use('/admin', require('./admin/adminDashboard'));
 app.use('/api/user', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profileRouter'));
 // individual profile routes
 app.use('/api/menteeProfile', require('./routes/api/menteeProfile'));
 app.use('/api/mentorProfile', require('./routes/api/mentorProfile'));
 app.use('/api/partnerProfile', require('./routes/api/partnerProfile'));
+app.use('/api/profile', require('./routes/api/profileRouter'));
 
 const PORT = process.env.PORT || 6005;
 app.listen(PORT, () => console.log(`Application running on ${PORT}`));
